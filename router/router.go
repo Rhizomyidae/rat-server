6ca6616a6ab02a37170ea9db2486e3dc0ecbe7f4
火星人 <450216797@qq.com>
2020-05-29 00:10:52 +0800

    fix init

--- router/router.go
+++ router/router.go
@@ -1,9 +1,5 @@
 package router
 
-import "github.com/gogf/gf/frame/g"
-
 // 用于配置初始化.
 func init() {
-	// Avoid 404 for some browsers requesting "/favicon.ico".
-	g.Server().SetRewrite("/favicon.ico", "/resource/image/favicon.ico")
 }
